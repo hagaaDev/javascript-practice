6404abab320b0774f103bf183ec38258e13d37ce
김선희 <sunkim@hist.co.kr>
2022-05-06 09:23:19 +0900

    함수 기초

---
+++ function-basic.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// 함수 선언
+function doSomething(add) {
+  console.log(add);
+  const result = add(2, 3);
+  console.log(result);
+}
+
+function add(a, b) {
+  const sum = a + b;
+  return sum;
+}
+
+// 함수 호출
+doSomething(add);
+
+const result = add(1, 2);
+console.log(result);
+
+const addFun = add;
+console.log(addFun);
+addFun(1, 2);
